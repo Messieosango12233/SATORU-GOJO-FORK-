caadd5d6dc2002e93913ee38c52f1f601dc2ffce
Messieosango12233 <satorum381@gmail.com>
2024-10-11 18:12:23 +0100

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,63 +1,84 @@
-€cmd install ai.js const axios = require('axios');
+const axios = require('axios');
 
 async function fetchFromAI(url, params) {
-  try {
-    const response = await axios.get(url, { params });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+ try {
+ const response = await axios.get(url, { params });
+ return response.data;
+ } catch (error) {
+ console.error(error);
+ return null;
+ }
 }
 
-async function getAIResponse(input, userId, messageID) {
-  const services = [
-    { url: 'https://ai-tools.replit.app/gpt', params: { prompt: input, uid: userId } },
-    { url: 'https://openaikey-x20f.onrender.com/api', params: { prompt: input } },
-    { url: 'http://fi1.bot-hosting.net:6518/gpt', params: { query: input } },
-    { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
-  ];
+async function getAIResponse(input, userName, userId, messageID) {
+ const services = [
+ { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
+ ];
 
-  let response = "𝔰𝔞𝔩𝔲𝔱, 𝔧𝔢 𝔰𝔲𝔦𝔰 𝔩'𝔦𝔫𝔱𝔢𝔩𝔩𝔦𝔤𝔢𝔫𝔠𝔢 𝔞𝔯𝔱𝔦𝔣𝔦𝔠𝔦𝔩𝔩𝔢 ℭℜéé 𝔭𝔞𝔯 𝔪𝔢𝔰𝔰𝔦𝔢 𝔬𝔰𝔞𝔫𝔤𝔬 𝔮𝔲𝔢 𝔭𝔲𝔦𝔰-𝔧𝔢 𝔣𝔞𝔦𝔯𝔢 𝔭𝔬𝔲𝔯 𝔳𝔬𝔲𝔰 ? ";
-  let currentIndex = 0;
+ let response = `🍀 ☆𝙎𝘼𝙏𝑂𝑅𝑈 𝔤𝔬𝔧𝔬  𝐵𝑂𝑇
+━━━━━━━━━━━━━━━━
+𝔰𝔞𝔩𝔲𝔱, 𝔧𝔢 𝔰𝔲𝔦𝔰 𝔩'𝔦𝔫𝔱𝔢𝔩𝔩𝔦𝔤𝔢𝔫𝔠𝔢 𝔞𝔯𝔱𝔦𝔣𝔦𝔠𝔦𝔩𝔩𝔢 ℭℜéé 𝔭𝔞𝔯 𝔪𝔢𝔰𝔰𝔦𝔢 𝔬𝔰𝔞𝔫𝔤𝔬 𝔮𝔲𝔢 𝔭𝔲𝔦𝔰-𝔧𝔢 𝔣𝔞𝔦𝔯𝔢 𝔭𝔬𝔲𝔯 𝔳𝔬𝔲𝔰 ? ( envoyé avec un effet de feu)
+━━━━━━━━━━━━━━━━ \n est active. Pose ta question 🍀`;
+ let currentIndex = 0;
 
-  for (let i = 0; i < services.length; i++) {
-    const service = services[currentIndex];
-    const data = await fetchFromAI(service.url, service.params);
-    if (data && (data.gpt4 || data.reply || data.response)) {
-      response = data.gpt4 || data.reply || data.response;
-      break;
-    }
-    currentIndex = (currentIndex + 1) % services.length; // Move to the next service in the cycle
-  }
+ for (let i = 0; i < services.length; i++) {
+ const service = services[currentIndex];
+ const data = await fetchFromAI(service.url, service.params);
+ if (data && (data.gpt4 || data.reply || data.response)) {
+ response = data.gpt4 || data.reply || data.response;
+ break;
+ }
+ currentIndex = (currentIndex + 1) % services.length; // Passer au service suivant
+ }
 
-  return { response, messageID };
+ return { response, messageID };
 }
 
 module.exports = {
-  config: {
-    name: 'ai',
-    author: 'Arn',
-    role: 0,
-    category: 'ai',
-    shortDescription: 'ai to ask anything',
-  },
-  onStart: async function ({ api, event, args }) {
-    const input = args.join(' ').trim();
-    if (!input) {
-      api.sendMessage(`SATAN II\n━━━━━━━━━━━━━━━━\nPlease provide a question or statement.\n━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
-      return;
-    }
+ config: {
+ name: 'ai',
+ author: 'shizuka',
+ role: 0,
+ aliase: ["ai"],
+ category: 'ai-chat',
+ shortDescription: 'ai to ask anything',
+ },
+ onStart: async function ({ api, event, args }) {
+ const input = args.join(' ').trim();
+ if (!input) {
+ api.sendMessage(" ☆𝙎𝘼𝙏𝑂𝑅𝑈 𝔤𝔬𝔧𝔬  𝐵𝑂𝑇
+━━━━━━━━━━━━━━━━
+𝔰𝔞𝔩𝔲𝔱, 𝔧𝔢 𝔰𝔲𝔦𝔰 𝔩'𝔦𝔫𝔱𝔢𝔩𝔩𝔦𝔤𝔢𝔫𝔠𝔢 𝔞𝔯𝔱𝔦𝔣𝔦𝔠𝔦𝔩𝔩𝔢 ℭℜéé 𝔭𝔞𝔯 𝔪𝔢𝔰𝔰𝔦𝔢 𝔬𝔰𝔞𝔫𝔤𝔬 𝔮𝔲𝔢 𝔭𝔲𝔦𝔰-𝔧𝔢 𝔣𝔞𝔦𝔯𝔢 𝔭𝔬𝔲𝔯 𝔳𝔬𝔲𝔰 ? 
+━━━━━━━━━━━━━━━━\n est active. Pose ta question", event.threadID, event.messageID);
+ return;
+ }
 
-    const { response, messageID } = await getAIResponse(input, event.senderID, event.messageID);
-    api.sendMessage(`DIEU OTOTSUKI \n━━━━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━━━━`, event.threadID, messageID);
-  },
-  onChat: async function ({ event, message }) {
-    const messageContent = event.body.trim().toLowerCase();
-    if (messageContent.startsWith("ai")) {
-      const input = messageContent.replace(/^ai\s*/, "").trim();
-      const { response, messageID } = await getAIResponse(input, event.senderID, message.messageID);
-      message.reply(`☆𝙎𝘼𝙏𝑂𝑅𝑈 𝔤𝔬𝔧𝔬  𝐵𝑂𝑇\n━━━━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━━━━`, messageID);
-    }
-  }
+ api.getUserInfo(event.senderID, async (err, ret) => {
+ if (err) {
+ console.error(err);
+ return;
+ }
+ const userName = ret[event.senderID].name;
+ const { response, messageID } = await getAIResponse(input, userName, event.senderID, event.messageID);
+ api.sendMessage(`GOD-TECH\n━━━━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━━━━☆𝙎𝘼𝙏𝑂𝑅𝑈 𝔤𝔬𝔧𝔬  𝐵𝑂T 
+━━━━━━━━━━━━━━━━ \n est active. Pose ta question`, event.threadID, messageID);
+ });
+ },
+ onChat: async function ({ api, event, message }) {
+ const messageContent = event.body.trim().toLowerCase();
+ if (messageContent.startsWith("ai")) {
+ const input = messageContent.replace(/^ai\s*/, "").trim();
+ api.getUserInfo(event.senderID, async (err, ret) => {
+ if (err) {
+ console.error(err);
+ return;
+ }
+ const userName = ret[event.senderID].name;
+ const { response, messageID } = await getAIResponse(input, userName, event.senderID, message.messageID);
+ message.reply(`DALMUDO\n━━━━━━━━━━━━━━━━\n${userName} , ${response} ━━━━━━━━━━━━━━━━ \n `, messageID);
+api.setMessageReaction("🍀", event.messageID, () => {}, true);
+
+ });
+ }
+ }
 };
